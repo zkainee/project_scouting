6ea7780f31a0bd1182d2e17cf8433d942225741a
Kaine <kainevommeren@gmail.com>
2021-10-22 12:39:53 +0200

    Patch 2.0
    Grote patch van onze website voor het project.

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,19 @@
     Date: 08-10-2021
     Subject: Website Scouting Jan van Hoof
 */
+
+// Berekent leeftijd Scouting Jan van Hoof  vanaf het jaar 1980
+var yearCreated = 1980;
+var d = new Date();
+var n = d.getFullYear();
+
+function getAge(year)
+{
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    age = currentYear - year;
+    return age;
+}
+var calulatedAge = getAge(yearCreated);
+
+document.getElementById("year_age").innerHTML = calulatedAge;
