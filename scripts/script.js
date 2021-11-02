d8d444abfbc2c5c643f93121857963b3552e2d35
Kaine <kainevommeren@gmail.com>
2021-11-02 14:42:39 +0100

    Added vragensysteem voor rangen

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,38 @@ function getAge(year)
     return age;
 }
 
-function vraagnaam()
+function questions()
 {
-var user_name = prompt("Wat is je naam?");
-console.log("Hallo " + user_name);
-var user_age = prompt("Wat is je leeftijd");
-console.log("U bent " + user_age + "jaren jong");
+    alert("Welkom bij de vragenlijst\nWe stellen een aantal vragen bij welke rang jij hoort.\nHiermee kunnen we je indelen bij een rang.\nSucces!");
+    /* Vraag aan gebruiker */
+    var name_question = prompt("Wat is je naam? ");
+    alert("Welkom " + name_question);
+    var age_question = prompt("Wat is je leeftijd? ");
+    
+    /* Checkt of de gebruiker lager dan een bepaalde leeftijd is en daarop zegt die bij welke rang je hoort. */ 
+    /* Bijv. iemand die 9 is komt bij de Welpen en iemand die 16 is komt bij de Scouts */
+    if (age_question < 11) 
+    {
+        document.getElementById("age_text").innerHTML = "Je hoort bij de Welpen!";
+    } else if (age_question < 15) 
+    {
+        document.getElementById("age_text").innerHTML = "Je hoort bij de Scouts!";
+    } else if (age_question < 18) 
+    {
+        document.getElementById("age_text").innerHTML = "Je hoort bij de Explorers!";  
+    } else if (age_question < 25) 
+    {
+        document.getElementById("age_text").innerHTML = "Je hoort bij de Pivo's!";
+    } else 
+    {
+        document.getElementById("age_text").innerHTML = "Foutief antwoord";
+    }
 
+    alert("Je bent " + age_question + " jaar");
+
+    /* Logging in console */
+    console.log(name_question);
+    console.log(age_question);
 }
 
 var yearCreated = 1980;
@@ -36,7 +61,3 @@ if (choice != true)
 {
     location.href="pages/notverified.html";
 }
-
-logo = document.getElementById("logo");
-logo.onclick = vraagnaam;
-
